7923f196f748b85f8cd37d63e55b532cacb103c6
SURAJ MAURYA <121401103+suraj2510@users.noreply.github.com>
2026-02-15 22:55:04 +0530

    fix: case sensitive model import

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 
 
 // SIGNUP
